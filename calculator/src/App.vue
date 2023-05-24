4e050ba3e65303b4d1ef0aac1901f2fefa1ba5db
roni.jasmin@yahoo.com <roni.jasmin@yahoo.com>
2023-05-24 20:33:36 +0200

    commit

---
+++ calculator/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg">
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator';
+export default {
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style >
+  .bg {
+    padding: 2rem;
+    background-color: gray;
+    width: 100vw;
+    height: 100vh;
+  }
+  html, body {
+      font-family: 'Sora', sans-serif;
+  }
+</style>
